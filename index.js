32ee6e093605f46bbe2508106c0920fdc27ddcfc
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-09-07 22:33:11 -0600

    commit tree e61b45608a9bc152aa6dade39949d2bb63972d71

--- index.js
+++ index.js
@@ -31,33 +31,33 @@ function createTimeOutEvent (time) {
     return this
 }
 
-function timeIn (record, date) {
-    return record.timeInEvents.find ( event => 
+function timeIn (date) {
+    return this.timeInEvents.find ( event => 
         event.date == date && event.type == 'TimeIn' 
         ).hour
 }
 
-function timeOut (record, date) {
-    return record.timeOutEvents.find ( event => 
+function timeOut (date) {
+    return this.timeOutEvents.find ( event => 
         event.date == date && event.type == 'TimeOut' 
         ).hour
 }
 
-function hoursWorkedOnDate (record, date) {
-    return (timeOut(record, date) - timeIn(record, date)) / 100
+function hoursWorkedOnDate (date) {
+    return (timeOut.call(this, date) - timeIn.call(this, date)) / 100
 }
 
-function wagesEarnedOnDate (record, date) {
-    return hoursWorkedOnDate (record, date) * record.payPerHour
+function wagesEarnedOnDate (date) {
+    return hoursWorkedOnDate.call (this, date) * this.payPerHour
 }
 
-function dates (record) {
-    return record.timeInEvents.map (event => event.date)
-}
+// function dates (record) {
+//     return record.timeInEvents.map (event => event.date)
+// }
 
-function wages (record) {
-    return dates(record).map (date => wagesEarnedOnDate(record, date))
-}
+// function wages (record) {
+//     return dates(record).map (date => wagesEarnedOnDate(record, date))
+// }
 
 // function allWagesFor (record) {
 //     return wages(record).reduce ( (acc, value) => {return acc + value}, 0)
@@ -69,7 +69,7 @@ function findEmployeeByFirstName (records, firstName) {
 
 function calculatePayroll (records) {
     return records.reduce ( (acc, record) => {
-        return acc + allWagesFor(record)
+        return acc + allWagesFor.call(record)
     }, 0)
 }
 
