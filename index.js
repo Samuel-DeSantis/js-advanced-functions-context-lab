77aafeb788c4fa964a953fa2e2ce6e667fcd85c5
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-09-07 20:32:04 -0600

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,77 @@
-/* Your Code Here */
+function createEmployeeRecord (employee) {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords (employees) {
+    return employees.map( employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent (time) {
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(time.split(' ')[1], 10),
+        date: time.split(' ')[0]
+    })
+    return this
+}
+
+function createTimeOutEvent (time) {
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(time.split(' ')[1], 10),
+        date: time.split(' ')[0]
+    })
+    return this
+}
+
+function timeIn (record, date) {
+    return record.timeInEvents.find ( event => 
+        event.date == date && event.type == 'TimeIn' 
+        ).hour
+}
+
+function timeOut (record, date) {
+    return record.timeOutEvents.find ( event => 
+        event.date == date && event.type == 'TimeOut' 
+        ).hour
+}
+
+function hoursWorkedOnDate (record, date) {
+    return (timeOut(record, date) - timeIn(record, date)) / 100
+}
+
+function wagesEarnedOnDate (record, date) {
+    return hoursWorkedOnDate (record, date) * record.payPerHour
+}
+
+function dates (record) {
+    return record.timeInEvents.map (event => event.date)
+}
+
+function wages (record) {
+    return dates(record).map (date => wagesEarnedOnDate(record, date))
+}
+
+// function allWagesFor (record) {
+//     return wages(record).reduce ( (acc, value) => {return acc + value}, 0)
+// }
+
+function findEmployeeByFirstName (records, firstName) {
+    return records.find ( record => record.firstName == firstName)
+}
+
+function calculatePayroll (records) {
+    return records.reduce ( (acc, record) => {
+        return acc + allWagesFor(record)
+    }, 0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
